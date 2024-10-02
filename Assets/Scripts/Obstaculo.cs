b4275ccba1795dd97b3b7befb358e5b5be9bfb11
Martinawww03 <martinawang03@gmail.com>
2024-10-02 16:28:22 +0200

    Update Obstaculo.cs

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -7,6 +7,9 @@ public class Obstaculo : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float velocidad = 0.5f;
     [SerializeField] int direccion;
+    [SerializeField] float timer = 0f;
+    bool alarmaActivada = true;
+
     void Start()
     {
 
@@ -15,7 +18,9 @@ public class Obstaculo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1, 0, 0 * velocidad * Time.deltaTime);
+        
+     transform.Translate(1, 0, 0 * velocidad * Time.deltaTime);
+
         
     }
 }
