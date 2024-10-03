79e7dcf624486dc0a072326e659333905c23eac9
Martinawww03 <martinawang03@gmail.com>
2024-10-03 19:21:30 +0200

    Update Apuntes.cs

--- Assets/Scripts/Apuntes.cs
+++ Assets/Scripts/Apuntes.cs
@@ -13,10 +13,13 @@ public class Apuntes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal"); //EN MAYUSCULA
         // A (h=-1), D (h=-1) , ->(h=1) , <- (h=-1)
 
-        float v = Input.GetAxisRaw("Vertical");
+        float v = Input.GetAxisRaw("Vertical"); // EN MAYUSCULA
         // S (v=-1), W (v=-1) , ^(v=1) , v (v=-1)
+
+        // Debug.Log(v);
+
     }
 }
