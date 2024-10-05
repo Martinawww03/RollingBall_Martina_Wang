edcee0da3a1a842666e53949e687945837e1b5ab
Martinawww03 <martinawang03@gmail.com>
2024-10-05 20:29:19 +0200

    movimiento de player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float velocidad = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 movimiento = new Vector3(h, 0.0f, v);
+
+        transform.Translate(movimiento);
+
     }
 }
