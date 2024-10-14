12257d1370d589d8c687cbb0fb092898f94c642a
Martinawww03 <martinawang03@gmail.com>
2024-10-14 19:09:59 +0200

    cambio de camara

--- Assets/Scripts/CambioCamara.cs
+++ Assets/Scripts/CambioCamara.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CambioCamara : MonoBehaviour
 {
+    [SerializeField]GameObject camAApagar;
+    [SerializeField]GameObject camEncender;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            camAApagar.SetActive(false);
+            camEncender.SetActive(true);
+
+        }
     }
 }
