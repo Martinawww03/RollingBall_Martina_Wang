9157a474fad5ea9088b5d95b42b7c70b8844cb11
Martinawww03 <martinawang03@gmail.com>
2024-10-16 17:06:57 +0200

    pared de piedra

--- Assets/Scripts/ParedPiedra.cs
+++ Assets/Scripts/ParedPiedra.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class ParedPiedra : MonoBehaviour
 {
+    private bool inciarTimer;
+    private float timer = 0;
+
+    private void Update()
+    {
+         if(inciarTimer==true)
+         {
+            timer += 1 * Time.unscaledDeltaTime; //no afecta a tiempo
+            if(timer >= 2)
+            {
+                Time.timeScale = 1;
+            }
+         }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+
             Time.timeScale = 0.2f;
+            inciarTimer = true;
+            
         }
     }
 }
