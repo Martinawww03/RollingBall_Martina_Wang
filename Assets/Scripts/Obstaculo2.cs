cf2aa74b6b30bedd207754d4864c7a09d5656d96
Martinawww03 <martinawang03@gmail.com>
2024-10-30 13:09:28 +0100

    obstaculo

--- Assets/Scripts/Obstaculo2.cs
+++ Assets/Scripts/Obstaculo2.cs
@@ -6,15 +6,16 @@ public class Obstaculo2 : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] private float fuerza = 0.5f;
+    [SerializeField] private Vector3 direccionRotacion;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddTorque((direccionRotacion).normalized * fuerza, ForceMode.VelocityChange);//que no le afecta la masa
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddTorque(new Vector3(0, 1, 0) * fuerza, ForceMode.Impulse);
     }
 }
